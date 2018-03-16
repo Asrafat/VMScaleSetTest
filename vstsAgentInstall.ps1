627efb0911c2242d730e6d5c1c5f0d3992f76ef0
AsraFatima <asra-fatima@outlook.com>
2018-03-16 15:46:53 -0700

    Update vstsAgentInstall.ps1

--- vstsAgentInstall.ps1
+++ vstsAgentInstall.ps1
@@ -4,8 +4,8 @@ cd \agent;
 $cwd = Get-Location;
 #md \vstsagent.zip
 
-$destinationFolder = Join-Path -Path $cwd -ChildPath "\vstsAgent.zip"
+#$destinationFolder = Join-Path -Path $cwd -ChildPath "\vstsAgent.zip"
 New-Item -ItemType Directory -Force -Path $destinationFolder | Out-Null
-#Invoke-WebRequest "https://github.com/Microsoft/vsts-agent/releases/download/v2.124.0/vsts-agent-win7-x64-2.124.0.zip" -OutFile "\vstsagent.zip" -UseBasicParsing
-#Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory($agent, "$cwd")
+Invoke-WebRequest "https://github.com/Microsoft/vsts-agent/releases/download/v2.124.0/vsts-agent-win7-x64-2.124.0.zip" -OutFile "\vstsagent.zip" -UseBasicParsing
+Add-Type -AssemblyName System.IO.Compression.FileSystem ; [System.IO.Compression.ZipFile]::ExtractToDirectory($agent, "$cwd")
 
